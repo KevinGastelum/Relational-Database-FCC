6d93eb1ec2a7ff06c85bafc079e2e58b7215cb4a
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2023-12-03 20:43:20 -0700

    Update worldcup.sql

---
+++ worldcup.sql
@@ -0,0 +1,20 @@
+-- CREATE DATABASE
+CREATE DATABASE worldcup;
+
+--CREATE TABLES
+
+-- Table Teams
+CREATE TABLE teams (
+    team_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE );
+-- Table Games
+CREATE TABLE games (
+    game_id SERIAL PRIMARY KEY,
+    year INT NOT NULL,
+    round VARCHAR(255) NOT NULL,
+    winner_id INT NOT NULL,
+    opponent_id INT NOT NULL,
+    winner_goals INT NOT NULL,
+    opponent_goals INT NOT NULL,
+    FOREIGN KEY (winner_id) REFERENCES teams(team_id),
+    FOREIGN KEY (opponent_id) REFERENCES teams(team_id));
